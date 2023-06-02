3388ac6f6398b904cd9f3c870c65a02fcc3775fc
damattag <guilherme.damatta@yahoo.com.br>
2023-06-02 00:06:40 -0300

    create service crud

--- server/src/models/Service.ts
+++ server/src/models/Service.ts
@@ -1,7 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 
 @Entity()
-export class Carousel {
+export class Service{
 
     @PrimaryGeneratedColumn()
     id: number
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import UserController from '@controllers/UserController'
 import ProcessController from '@controllers/ProcessController';
+import ServiceController from '@controllers/ServiceController';
 
 const routes = express.Router();
 const userController = new UserController();
 const processController = new ProcessController();
+const serviceController = new ServiceController();
 
 routes.post('/user', userController.create);
 routes.get('/user', userController.get);
@@ -16,5 +18,9 @@ routes.get('/process', processController.get);
 routes.delete('/process/:id', processController.delete);
 routes.put('/process/:id', processController.update);
 
+routes.post('/service', serviceController.create);
+routes.get('/service', serviceController.get);
+routes.delete('/service/:id', serviceController.delete);
+routes.put('/service/:id', serviceController.update);
 
 export default routes;
