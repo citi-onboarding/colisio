3cff6f4e3f4f58d005532c0e23bddb5ebe74dd2a
lucas <teixeiralucas940@outlook.com>
2023-06-03 16:35:01 -0300

    Correção de div. Correção da biblioteca e implementação de flexbox

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -7,26 +7,19 @@ import { ImgHome } from '../../assets';
 export const Home: React.FC = () => {
     return (
         <HomeContainerdiv id="start">
-            <div>
-                <HomeH1>Construa times de Marketing sob demanda</HomeH1>
-                <HomeP>Concentre-se na sua estratégia e colabore com profissionais testados e selecionados com as habilidades específicas que seu negócio precisa.</HomeP>
-            </div>
-            <div>
-                <ButtonH>
-                    <Link href="#Contact">
-                        <Button>
-                            <Contato>Comece agora</Contato>
-                        </Button>
-                    </Link>
-                </ButtonH>
-            </div>
-            <div>
-                <HomeImg
-                    src={ImgHome}
-                    alt=''
-                />
-            </div>
-            <div id="Contact"></div>
+            <HomeH1>Construa times de Marketing sob demanda</HomeH1>
+            <HomeP>Concentre-se na sua estratégia e colabore com profissionais testados e selecionados com as habilidades específicas que seu negócio precisa.</HomeP>
+            <ButtonH>
+                <Link href="#Contact">
+                    <Button>
+                        <Contato>Comece agora</Contato>
+                    </Button>
+                </Link>
+            </ButtonH>
+            <HomeImg
+                src={ImgHome}
+                alt=''
+            />
         </HomeContainerdiv>
     )
 
--- client/src/pages/home/styles.ts
+++ client/src/pages/home/styles.ts
@@ -8,9 +8,10 @@ export const HomeContainerdiv = styled.div`
     margin-left: 0;
     margin-right: 0;
     display: flex;
-    justify-content: center;
     align-items: center;
     flex-direction: column;
+    justify-content: space-evenly;
+    text-align: center;
     `;
 
 export const HomeH1 = styled.h1`
@@ -18,37 +19,21 @@ export const HomeH1 = styled.h1`
     font-weight: 700; //pegando um único weight para o HomeP e HomeH1
     font-size: 64px;
     color: #000000;
-    text-align: center;
-    margin-top: 0px;
-    margin-left: 279px;
-    margin-right: 279px;
-    padding: 0px;
-    padding-top: 65px;
-    padding-bottom: 39px;
-    line-height: 75px;
     `;
 
 export const HomeP = styled.p`
     font-family: 'Raleway';
-    width: 961px;
-    height: 105px;
     font-family: 'Raleway';
     font-style: normal;
     font-weight: 400; //pegando um único weight para o HomeP e HomeH1
     font-size: 24px;
     line-height: 38px;
-    text-align: center;
-    color: #626262;
-    padding-bottom: 39px;
-    margin-left: 240px;
-    margin-right: 240px;
-    
+    color: #626262;  
     `;
 
 export const HomeImg = styled.img`
     width: 1080px;
     height: 616px;
-    display: flex;
     `;
 
 //Constante para botão
@@ -63,13 +48,10 @@ export const Button = styled.button`
     flex-direction: row;
     justify-content: center;
     align-items: center;
-    margin-bottom: 69px;
-    padding: 6px 32px;
     gap: 10px;
     background-color: #05B055;
     border: none;
     color: #F5F5F5;
-
     &:hover {
         color: #F5F5F5;
         background-color: #12F87D;
@@ -78,11 +60,12 @@ export const Button = styled.button`
 `;
 
 export const Contato = styled.div`
-    `;
+`;
 
 export const Link = styled.a`
     text-decoration: none;
-`
+`;
+
 export const ButtonH = styled.div`
     
-`
+`;
