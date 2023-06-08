52a43402457c08f584e5cb0e03f49442b6288d24
marcos-gma <marcosgabrielma5@gmail.com>
2023-06-08 19:24:37 -0300

    Added the nodemailer

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import UserController from '@controllers/UserController'
+import { SendMail } from '@controllers/MailController';
 
 const routes = express.Router();
 const userController = new UserController();
@@ -8,6 +9,8 @@ routes.post('/user', userController.create);
 routes.get('/user', userController.get);
 routes.delete('/user/:id', userController.delete);
 routes.put('/user/:id', userController.update);
+
+routes.post('/email', SendMail);
 
 
 export default routes;
