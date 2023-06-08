964012c6e4d6f78102fefabee7dcee02ecb0ca39
João Luís <jlga@cin.ufpe.br>
2023-06-08 15:19:14 -0300

    Feature(crud-carousel-2): Crud working with its bugs fixed

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,21 @@
 import express from 'express';
 import UserController from '@controllers/UserController'
+import CarouselController from '@controllers/CarouselController';
 
 const routes = express.Router();
 const userController = new UserController();
+const carouselController = new CarouselController();
 
 routes.post('/user', userController.create);
 routes.get('/user', userController.get);
 routes.delete('/user/:id', userController.delete);
 routes.put('/user/:id', userController.update);
+
+routes.post('/carousel', carouselController.create);
+routes.get('/carousel', carouselController.get);
+routes.delete('/carousel/:id', carouselController.delete);
+routes.put('/carousel/:id', carouselController.update);
+
 
 
 export default routes;
