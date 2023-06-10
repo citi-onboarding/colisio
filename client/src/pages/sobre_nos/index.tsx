7ea5af9fe45b3b7562cd621062cde6dbc16d73b7
marcos-gma <marcosgabrielma5@gmail.com>
2023-06-09 21:35:11 -0300

    Created the section: About Us

--- client/src/pages/sobre_nos/index.tsx
+++ client/src/pages/sobre_nos/index.tsx
@@ -1,24 +1,32 @@
 import React from "react";
 import { SobreNos, LogoColisio2 } from "../../assets";
-import { Foto, SobreNosContainer, Titulo, Slogan } from "./styles";
+import { Foto, SobreNosContainer, Titulo, Slogan, Logo, Texto, Conteudo, Parte_Escrita, Caminho } from "./styles";
 
 
 export const Sobre_Nos: React.FC = () => {
     return(
+
         <SobreNosContainer>
             <Titulo>
                 Sobre nós
             </Titulo>
-            <Foto>
-                <img src={SobreNos} width={400} height={516.63}/>
-            </Foto>
-            <Slogan>
-                Não somos uma agência.
-                Somos um caminho.
-            </Slogan>
-            <Logo>
-                <img src={LogoColisio2} width={499} height={79}/>
-            </Logo>
+            <Conteudo>
+                <Foto>
+                    <img src={SobreNos} width={400} height={516.63}/>
+                </Foto>
+                <Parte_Escrita>
+                    <Slogan>
+                        Não somos uma agência.
+                        Somos um <Caminho>caminho.</Caminho>
+                    </Slogan>
+                    <Texto>
+                        A Colisio é uma startup que se encontra entre a colisão e a colaboração. Utilizamos metodologias próprias, ferramentas e práticas inovadoras para mostrar o caminho para quem não sabe onde ir.
+                    </Texto>
+                </Parte_Escrita>
+                <Logo>
+                    <img src={LogoColisio2} width={675} height={165}/>
+                </Logo>
+            </Conteudo>
         </SobreNosContainer>
 
     )
